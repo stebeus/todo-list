9117d88ddee8c3680785253b73d876790fd95256
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-21 10:29:08 -0300

    feat: install Webpack html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };

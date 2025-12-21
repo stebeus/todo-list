4d7ae5e0cef2b4f15e02e57ce27ba7fa0b348754
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-21 15:44:15 -0300

    feat: add id property to task factory

--- src/components/logic.js
+++ src/components/logic.js
@@ -1,10 +1,13 @@
 import { generateID, convertDateToObj } from "../utils/helpers";
 
 function createTask(name, desc, priority, dueDate) {
+  const id = generateID();
+
   let isComplete = false;
   const toggleComplete = () => (isComplete = !isComplete);
 
   return {
+    id,
     name,
     desc,
     priority,

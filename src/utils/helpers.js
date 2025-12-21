6cb879c29845a6825fdc091c6d60a96af965d530
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-21 15:41:32 -0300

    feat: create convertDateToObj helper

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,4 @@
 const generateID = () => crypto.randomUUID().replace(/-/g, "").slice(0, 10);
+const convertDateToObj = (str) => (str ? new Date(str) : null);
 
-export { generateID };
+export { generateID, convertDateToObj };

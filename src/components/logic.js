77663582a61d0f8d27e4206e3474698e2f123388
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-21 15:37:09 -0300

    feat: create task factory

--- src/components/logic.js
+++ src/components/logic.js
@@ -1,1 +1,19 @@
-export {};
+function createTask(name, desc, priority, dueDate) {
+  let isComplete = false;
+  const toggleComplete = () => (isComplete = !isComplete);
+
+  return {
+    name,
+    desc,
+    priority,
+    get dueDate() {
+      return dueDate;
+    },
+    get isComplete() {
+      return isComplete;
+    },
+    toggleComplete,
+  };
+}
+
+export { createTask };

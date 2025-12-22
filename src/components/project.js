9f9758c101627dfa1c2df6361c89a6030aa2f933
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-22 08:30:48 -0300

    feat: create clearTaskList function

--- src/components/project.js
+++ src/components/project.js
@@ -6,5 +6,6 @@ const taskList = document.querySelector(".task-list");
 let currProject = createProject("Home");
 
 const submitTask = (...values) => currProject.add(createTask(...values));
+const clearTaskList = () => (taskList.innerHTML = "");
 
 export { taskList, currProject, submitTask };

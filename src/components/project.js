2615cc34cd675f0897e8070237303c0a68b7c63b
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-22 07:59:39 -0300

    feat: query select task-list

--- src/components/project.js
+++ src/components/project.js
@@ -1,5 +1,7 @@
 import { createTask, createProject, projectManager } from "./logic";
 
+const taskList = document.querySelector(".task-list");
+
 let currProject = createProject("Home");
 
 export { currProject };

db6ecf2e60a20d3791ad382fd678bba24279fe73
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-22 12:40:42 -0300

    feat: create initHome function

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,4 +2,9 @@ import { createProject, projectManager } from "./logic";
 
 const submitProject = (name) => projectManager.add(createProject(name));
 
+function initHome() {
+  const home = createProject("Home");
+  submitProject(home);
+}
+
 export { submitProject };

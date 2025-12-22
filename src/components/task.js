f9b886f8e5fc7d8c4707aae7e0177ea3d751d079
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-22 08:26:36 -0300

    feat: create renderTask function

--- src/components/task.js
+++ src/components/task.js
@@ -1,3 +1,17 @@
 import { taskList } from "./project";
 
-export {};
+function renderTask(task) {
+  const template = document.querySelector("#task-item-template");
+  const clone = template.content.cloneNode(true);
+
+  clone.querySelector(".task-item").dataset.id = task.id;
+  clone.querySelector("h4").textContent = task.name;
+  clone.querySelector("#desc").textContent = task.desc;
+  clone.querySelector("#due-date").textContent = task.dueDate;
+  clone.querySelector("#priority").textContent = task.priority;
+  clone.querySelector("#status").textContent = task.isComplete;
+
+  taskList.appendChild(clone);
+}
+
+export { renderTask };

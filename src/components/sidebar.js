777d89d457b9660a2aa8f3cfa6020a6bdbad10c0
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-22 12:46:41 -0300

    fix: return home in initHome

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -5,6 +5,7 @@ const submitProject = (name) => projectManager.add(createProject(name));
 function initHome() {
   const home = createProject("Home");
   submitProject(home);
+  return home;
 }
 
 let currProject = initHome();

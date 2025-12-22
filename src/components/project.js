42ec023fb0d46994295dfa4f1d151cc3b3f7d33d
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-22 08:33:21 -0300

    feat: create updateTaskList function

--- src/components/project.js
+++ src/components/project.js
@@ -8,4 +8,9 @@ let currProject = createProject("Home");
 const submitTask = (...values) => currProject.add(createTask(...values));
 const clearTaskList = () => (taskList.innerHTML = "");
 
-export { taskList, currProject, submitTask };
+function updateTaskList() {
+  clearTaskList();
+  currProject.list.forEach(renderTask);
+}
+
+export { taskList, currProject, submitTask, updateTaskList };

f8bde61f247e2db7d0d67d0bce0284dce41eaf95
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-23 08:08:27 -0300

    feat!: move button handlers to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 import "./style.css";
-import { handleBtns } from "./components/buttons";
+import { addProject, switchProject, removeProject } from "./components/sidebar";
+import { addTask, removeTask } from "./components/project";
+import { switchPriority, switchStatus } from "./components/task";
+import { close } from "./components/modal";
+
+function handleBtns(e) {
+  const btn = e.target.closest("button");
+  if (!btn) return;
+
+  const { action } = btn.dataset;
+
+  const actionHandlers = {
+    "add-project": addProject,
+    "switch-project": switchProject,
+    "remove-project": removeProject,
+    "add-task": addTask,
+    "switch-priority": switchPriority,
+    "switch-status": switchStatus,
+    "remove-task": removeTask,
+    close,
+  };
+
+  const actionHandler = actionHandlers[action];
+  if (actionHandler) return actionHandler(e);
+}
 
 document.addEventListener("click", handleBtns);

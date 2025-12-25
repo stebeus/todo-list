01423e334c092f4cb6c8d751c415996591246fcb
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-25 14:23:48 -0300

    feat: remove storeTaskData function

--- src/storage.js
+++ src/storage.js
@@ -1,10 +1,4 @@
 const storeProjectData = (key, value) =>
   localStorage.setItem(key, JSON.stringify(value));
 
-function storeTaskData(key, value) {
-  value.forEach((project) => {
-    localStorage.setItem(key, JSON.stringify(project.list));
-  });
-}
-
-export { storeProjectData, storeTaskData };
+export { storeProjectData };

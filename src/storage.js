4c51f1602fc43eed9b647b70f507f238a678335c
stebeus <189894010+stebeus@users.noreply.github.com>
2025-12-25 15:47:20 -0300

    feat: set projectManager and currProject to local data

--- src/storage.js
+++ src/storage.js
@@ -1,11 +1,13 @@
+import { projectManager } from "./components/logic";
+import { currProject } from "./components/sidebar";
+
 const storeProjectData = (key, value) =>
   localStorage.setItem(key, JSON.stringify(value));
 
 function retrieveData() {
   if (localStorage.length > 0) {
-    localStorage.getItem("project-list");
-    localStorage.getItem("current-project");
-    console.log(localStorage);
+    projectManager.list = JSON.parse(localStorage.getItem("project-list"));
+    currProject = JSON.parse(localStorage.getItem("current-project"));
   }
 }
 
